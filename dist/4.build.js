db6ab82ed44716ec13cde081ef9c14736c9484fb
hilongjw <hilongjw@qq.com>
2016-01-09 13:05:43 +0800

    update

---
+++ dist/4.build.js
@@ -0,0 +1,6 @@
+/*!
+ * This file is created by longjw 
+ *  email : hilongjw@gmail.com 
+ *  updated_at : Sat Jan 09 2016 13:03:30 GMT+0800 (中国标准时间)
+ */
+webpackJsonp([4],{65:function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t["default"]={}},137:function(e,t){e.exports="<h1>32432asdsafsdf</h1>"},144:function(e,t,o){var s,p;s=o(65),p=o(137),e.exports=s||{},e.exports.__esModule&&(e.exports=e.exports["default"]),p&&(("function"==typeof e.exports?e.exports.options:e.exports).template=p)}});
