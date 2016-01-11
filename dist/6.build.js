59ae57bc64a0b27c230339e1265648af8692f30c
hilongjw <hilongjw@qq.com>
2016-01-11 13:00:40 +0800

    update

--- dist/6.build.js
+++ dist/6.build.js
@@ -1,6 +1,6 @@
 /*!
  * This file is created by longjw 
  *  email : hilongjw@gmail.com 
- *  updated_at : Mon Jan 11 2016 11:24:42 GMT+0800 (中国标准时间)
+ *  updated_at : Mon Jan 11 2016 12:57:46 GMT+0800 (中国标准时间)
  */
 webpackJsonp([6],{66:function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t["default"]={}},143:function(e,t){e.exports="<h1>32432asdsafsdf</h1>"},151:function(e,t,o){var s,p;s=o(66),p=o(143),e.exports=s||{},e.exports.__esModule&&(e.exports=e.exports["default"]),p&&(("function"==typeof e.exports?e.exports.options:e.exports).template=p)}});
